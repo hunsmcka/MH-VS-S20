46fc79de49175b84db38add97783d531f879dc8f
McKay Hunsaker <60007606+hunsmcka@users.noreply.github.com>
2020-01-21 21:32:04 -0700

    Add files via upload

---
+++ MoreOperators.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+Option Strict On
+
+Module MoreOperators
+
+    Sub Main()
+
+        'prompt user for a message
+        'then display the message
+        Dim userMessage As String
+
+        'userMessage = "Hello"
+
+        Console.WriteLine("Please enter a message. Press enter when done")
+        userMessage = Console.ReadLine()
+        Console.WriteLine("You entered " & Chr(34) & userMessage & Chr(34) & ". Good job!")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
