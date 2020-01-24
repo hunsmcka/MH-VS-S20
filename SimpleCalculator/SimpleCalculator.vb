100b70bec243802d33282d78027502b10dd349cd
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-01-24 07:58:21 -0800

    Create simple calculator.

---
+++ SimpleCalculator/SimpleCalculator.vb
@@ -0,0 +1,19 @@
+﻿Option Explicit On
+Option Strict On
+
+Module SimpleCalculator
+
+    Sub Main()
+
+        Dim firstNumber As String
+        Dim secondNumber As String
+
+        firstNumber = Console.ReadLine()
+        secondNumber = Console.ReadLine()
+
+        Console.WriteLine(CInt(firstNumber) + CInt(secondNumber))
+        Console.ReadLine()
+
+    End Sub
+
+End Module
