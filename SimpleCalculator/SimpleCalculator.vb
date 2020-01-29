e16930a27df0ae5a2e670ff2741cb424653ca538
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-01-29 08:00:42 -0800

    Fix bugs with user inputing string where there should be integers and loop them back through the program when there's an error or if the user wants to.

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -12,44 +12,77 @@ Module SimpleCalculator
 
     Sub Main()
 
-        Dim firstNumber As String
-        Dim secondNumber As String
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
         Dim mathAction As String
+        Dim quitChoice As Boolean
+        Dim promtUser As Boolean
 
-        'prompt user for two numbers
-        Console.WriteLine("Please enter a number and press enter.")
-        firstNumber = Console.ReadLine()
+        quitChoice = False
 
-        Console.WriteLine("Please enter a second number and press enter.")
-        secondNumber = Console.ReadLine()
+        Do While quitChoice = False
+            promtUser = True
 
-        'prompt user for operator type
-        Console.WriteLine("Choose one of the following action:")
-        Console.WriteLine("1. Add")
-        Console.WriteLine("2. Multiply")
+            Do While promtUser = True
+                'prompt user for two numbers
+                Console.WriteLine("Please enter a number and press enter.")
+                Try
+                    firstNumber = CInt(Console.ReadLine())
+                    promtUser = False
+                Catch ex As InvalidCastException
+                    Console.WriteLine("Please enter a whole number.")
+                    promtUser = True
+                Catch ex As Exception
+                    Console.WriteLine("You broke my program.")
+                    promtUser = True
+                End Try
+            Loop
 
-        mathAction = Console.ReadLine()
+            Do While promtUser = False
+                Console.WriteLine("Please enter a second number and press enter.")
+                Try
+                    secondNumber = CInt(Console.ReadLine())
+                    promtUser = True
+                Catch ex As InvalidCastException
+                    Console.WriteLine("Please enter a whole number.")
+                    promtUser = False
+                Catch ex As Exception
+                    Console.WriteLine("You broke my program.")
+                    promtUser = False
+                End Try
+            Loop
 
-        'perform operation base on user's input
-        If mathAction = "1" Then
-            Try
-                Console.WriteLine(firstNumber & "+" & secondNumber & "=" & CInt(firstNumber) + CInt(secondNumber))
-            Catch ex As Exception
-                Console.WriteLine("Please enter a whole number.")
-                Console.WriteLine(ex.Message.ToString)
-            End Try
-        ElseIf mathAction = "2" Then
-            Try
-                Console.WriteLine(firstNumber & "*" & secondNumber & "=" & CInt(firstNumber) * CInt(secondNumber))
-            Catch ex As Exception
-                Console.WriteLine("Please enter a whole number.")
-                Console.WriteLine(ex.Message.ToString)
-            End Try
-        Else
-            Console.WriteLine("I'm sorry, that option is invalid. Please try again.")
-        End If
+            'prompt user for operator type
+            Console.WriteLine("Choose one of the following action:")
+            Console.WriteLine("1. Add")
+            Console.WriteLine("2. Multiply")
 
-        'display result prompt to end
+            mathAction = Console.ReadLine()
+
+            'perform operation base on user's input 
+            If mathAction = "1" Then
+                Console.WriteLine(firstNumber & "+" & secondNumber & "=" & firstNumber + secondNumber)
+
+            ElseIf mathAction = "2" Then
+                Console.WriteLine(firstNumber & "*" & secondNumber & "=" & firstNumber * secondNumber)
+
+            Else
+                Console.WriteLine("I'm sorry, that option is invalid. Please try again.")
+
+            End If
+
+            'promp user if they want to run the program again or quit
+            Console.WriteLine("Press Enter to run again. Press Q and Enter to run again.")
+
+            If Console.ReadLine() = "Q" Then
+                quitChoice = True
+            Else
+                quitChoice = False
+                Console.Clear()
+            End If
+
+        Loop
+
         Console.WriteLine("Thank you have a nice day.")
         Console.ReadLine()
 
