e18500d158cc0280f72872b675d3c1f09c5eefa8
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-01-30 07:57:19 -0800

    Create SubFunctionExample.

---
+++ SubFunctionExample/SubFunctionExample.vb
@@ -0,0 +1,41 @@
+﻿Option Explicit On
+Option Infer On
+Option Compare Binary
+Option Strict On
+Module SubFunctionExample
+
+    Sub Main()
+        Dim myNumber As Integer
+        myNumber = 7
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        MySub(myNumber)
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        myNumber = MyFunc(myNumber)
+        Console.WriteLine("In Sub Main")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+
+    End Sub
+
+    Sub MySub(myNumber As Integer)
+        'myNumber = myNumber + 2
+        myNumber += 2
+        Console.WriteLine("In Sub MySub")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+    End Sub
+
+    Function MyFunc(myNumber As Integer) As Integer
+        'myNumber = myNumber - 3
+        myNumber -= 3
+        Console.WriteLine("In Function MyFunc")
+        Console.WriteLine(myNumber)
+        Console.ReadLine()
+        Return myNumber
+    End Function
+
+End Module
