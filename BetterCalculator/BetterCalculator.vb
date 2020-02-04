a15fc480f9a4471f6c3329d4fd7a7f78b330c117
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-02-04 07:13:04 -0800

    Started BetterCalculator program

---
+++ BetterCalculator/BetterCalculator.vb
@@ -0,0 +1,25 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+'McKay Hunsaker
+'RCET0265
+'Spring 2020
+'Better Calculator
+'https://github.com/rosstimo/MH-VS-S20
+
+Module BetterCalculator
+
+    Sub Main()
+
+        Dim firstNumber As Integer
+        Dim secondNumber As Integer
+        Dim mathChoice As String
+        Dim quitProgram As Boolean
+
+        quitProgram = False
+
+    End Sub
+
+End Module
