fe8ee0c7104c6045aa35c0134e5ac6397e2df6fd
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-02-12 09:00:40 -0700

    Started the Validate User Data Form.

---
+++ ValidateUserData/ValidateUserData.vb
@@ -0,0 +1,21 @@
+﻿Public Class ValidateUserData
+    Private Sub ValidateButton_Click(sender As Object, e As EventArgs) Handles ValidateButton.Click
+
+        IsEmpty()
+
+    End Sub
+
+    Sub IsEmpty()
+        Dim userMessage As String
+        If TextBox1.Text = "" Then
+            userMessage = "Please enter text in textbox 1."
+            TextBox1.Select()
+        End If
+
+        If TextBox2.Text = "" Then
+            userMessage = "Please enter text in textbox 2."
+            TextBox2.Select()
+        End If
+    End Sub
+
+End Class
