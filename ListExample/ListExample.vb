a8f468ad84be82a86e04572d9939c2a2747faea5
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-02-20 08:44:23 -0700

    Created ListExample

---
+++ ListExample/ListExample.vb
@@ -0,0 +1,27 @@
+﻿Module ListExample
+
+    Sub Main()
+        Dim names As New List(Of String)
+        Dim searchString As String
+
+        names.Add("Yoda")
+        names.Add("Anakin")
+
+        'names.Add(Console.ReadLine())
+
+        searchString = Console.ReadLine()
+        If names.Contains(searchString) = True Then
+            Console.WriteLine("Yes, " & searchString & " is at index " & names.IndexOf(searchString))
+        Else
+            Console.WriteLine("Sorry, " & searchString & " was not found.")
+        End If
+
+        'For Each name In names
+        '    Console.WriteLine(name)
+        'Next
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
