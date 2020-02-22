f4aef30b12955932e17a1411b5083e44b644d5e0
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-02-22 11:24:51 -0700

    Connected all the buttons to Display Special Label

--- DinerMenu/DinerMenu.Designer.vb
+++ DinerMenu/DinerMenu.Designer.vb
@@ -22,25 +22,81 @@ Partial Class DinerMenuForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
-        'Label1
+        'DinerNameLabel
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 22.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(377, 30)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(260, 43)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Finney's Diner"
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Times New Roman", 22.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(300, 39)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(265, 43)
+        Me.DinerNameLabel.TabIndex = 0
+        Me.DinerNameLabel.Text = "Freddy's Diner"
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(12, 442)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(141, 115)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "&Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(211, 442)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(141, 115)
+        Me.SoupButton.TabIndex = 2
+        Me.SoupButton.Text = "&Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(421, 442)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(141, 115)
+        Me.FishButton.TabIndex = 3
+        Me.FishButton.Text = "&Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(710, 442)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(141, 115)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.AutoSize = True
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Times New Roman", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(101, 105)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(0, 23)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'DinerMenuForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1099, 569)
-        Me.Controls.Add(Me.Label1)
+        Me.ClientSize = New System.Drawing.Size(859, 569)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.DinerNameLabel)
         Me.Name = "DinerMenuForm"
         Me.Text = "Finney's Diner Special Menu"
         Me.ResumeLayout(False)
@@ -48,5 +104,10 @@ Partial Class DinerMenuForm
 
     End Sub
 
-    Friend WithEvents Label1 As Label
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DisplaySpecialLabel As Label
 End Class
--- DinerMenu/DinerMenu.vb
+++ DinerMenu/DinerMenu.vb
@@ -1,5 +1,50 @@
-﻿Public Class DinerMenuForm
-    Private Sub DinerMenuForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
 
+'McKay Hunsaker
+'RCET0265
+'Spring 2020
+'Diner Menu Program
+'https://github.com/hunsmcka/MH-VS-S20
+
+Public Class DinerMenuForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
     End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        DisplaySpecialLabel.Text = "~Fresh, Fresher, Freshest Fish~
+
+            Do you like fish? It's just fine at Freddy's diner 
+            Freddy also has some fresher fish that's fresher and much finer 
+            but his best fish is his freshest fish and Freddy says with pride
+            'The finest fish at Freddy's is my freshest fish, french-fried!'
+            SO... don't order the fresh or the fresher fish
+            at Finney's if you're wise, you'll say, 'fetch me the finest
+            french-fried freshest fish that Finney fries!'"
+    End Sub
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        DisplaySpecialLabel.Text = "               ~Soup of the Day~
+
+                Bisque de Crevettes 
+                A traditional recipe from the Charente region of France"
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        DisplaySpecialLabel.Text = "           ~Special Foster's Salad~
+            
+            We begin with a sprig of the finest Austailian parsley hand
+            selected by our head chef's, wife's, best-friend's, sister-in-law.
+            The premium parsley is plated on a bed of grilled to order
+            T-bone steak and fianlly garnished with an ice cold
+            can of Foster's Homemade Steak Sauce.
+
+            ~Desert options Include~
+
+            1 Cookie"
+    End Sub
+
 End Class
