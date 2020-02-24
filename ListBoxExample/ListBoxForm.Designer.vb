64136a980d91cc754dc0306ea51cbe7b0a1b0bb2
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-02-24 08:54:26 -0700

    Add some stuff

--- ListBoxExample/ListBoxForm.Designer.vb
+++ ListBoxExample/ListBoxForm.Designer.vb
@@ -23,68 +23,114 @@ Partial Class ListBoxForm
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DataListBox = New System.Windows.Forms.ListBox()
-        Me.AddButton = New System.Windows.Forms.Button()
-        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.AddItemButton = New System.Windows.Forms.Button()
+        Me.ClearAllButton = New System.Windows.Forms.Button()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameTextBox = New System.Windows.Forms.TextBox()
+        Me.FistNameLabel = New System.Windows.Forms.Label()
+        Me.LastNameLabel = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'DataListBox
         '
+        Me.DataListBox.Font = New System.Drawing.Font("Consolas", 7.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.DataListBox.FormattingEnabled = True
-        Me.DataListBox.ItemHeight = 16
-        Me.DataListBox.Location = New System.Drawing.Point(64, 12)
+        Me.DataListBox.ItemHeight = 15
+        Me.DataListBox.Location = New System.Drawing.Point(56, 84)
         Me.DataListBox.Name = "DataListBox"
-        Me.DataListBox.Size = New System.Drawing.Size(617, 340)
+        Me.DataListBox.Size = New System.Drawing.Size(714, 259)
         Me.DataListBox.TabIndex = 0
         Me.DataListBox.TabStop = False
         '
-        'AddButton
+        'AddItemButton
         '
-        Me.AddButton.Location = New System.Drawing.Point(64, 377)
-        Me.AddButton.Name = "AddButton"
-        Me.AddButton.Size = New System.Drawing.Size(130, 61)
-        Me.AddButton.TabIndex = 1
-        Me.AddButton.Text = "Add"
-        Me.AddButton.UseVisualStyleBackColor = True
+        Me.AddItemButton.Location = New System.Drawing.Point(334, 377)
+        Me.AddItemButton.Name = "AddItemButton"
+        Me.AddItemButton.Size = New System.Drawing.Size(130, 61)
+        Me.AddItemButton.TabIndex = 1
+        Me.AddItemButton.Text = "&Add Item"
+        Me.AddItemButton.UseVisualStyleBackColor = True
         '
-        'ClearButton
+        'ClearAllButton
         '
-        Me.ClearButton.Location = New System.Drawing.Point(215, 377)
-        Me.ClearButton.Name = "ClearButton"
-        Me.ClearButton.Size = New System.Drawing.Size(130, 61)
-        Me.ClearButton.TabIndex = 2
-        Me.ClearButton.Text = "Clear"
-        Me.ClearButton.UseVisualStyleBackColor = True
+        Me.ClearAllButton.Location = New System.Drawing.Point(485, 377)
+        Me.ClearAllButton.Name = "ClearAllButton"
+        Me.ClearAllButton.Size = New System.Drawing.Size(130, 61)
+        Me.ClearAllButton.TabIndex = 2
+        Me.ClearAllButton.Text = "&Clear All"
+        Me.ClearAllButton.UseVisualStyleBackColor = True
         '
         'ExitButton
         '
         Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.ExitButton.Location = New System.Drawing.Point(370, 377)
+        Me.ExitButton.Location = New System.Drawing.Point(640, 377)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(130, 61)
         Me.ExitButton.TabIndex = 3
         Me.ExitButton.Text = "E&xit"
         Me.ExitButton.UseVisualStyleBackColor = True
         '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(87, 26)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(241, 22)
+        Me.FirstNameTextBox.TabIndex = 4
+        '
+        'LastNameTextBox
+        '
+        Me.LastNameTextBox.Location = New System.Drawing.Point(491, 29)
+        Me.LastNameTextBox.Name = "LastNameTextBox"
+        Me.LastNameTextBox.Size = New System.Drawing.Size(241, 22)
+        Me.LastNameTextBox.TabIndex = 5
+        '
+        'FistNameLabel
+        '
+        Me.FistNameLabel.AutoSize = True
+        Me.FistNameLabel.Location = New System.Drawing.Point(5, 29)
+        Me.FistNameLabel.Name = "FistNameLabel"
+        Me.FistNameLabel.Size = New System.Drawing.Size(76, 17)
+        Me.FistNameLabel.TabIndex = 6
+        Me.FistNameLabel.Text = "First Name"
+        '
+        'LastNameLabel
+        '
+        Me.LastNameLabel.AutoSize = True
+        Me.LastNameLabel.Location = New System.Drawing.Point(409, 29)
+        Me.LastNameLabel.Name = "LastNameLabel"
+        Me.LastNameLabel.Size = New System.Drawing.Size(76, 17)
+        Me.LastNameLabel.TabIndex = 7
+        Me.LastNameLabel.Text = "Last Name"
+        '
         'ListBoxForm
         '
-        Me.AcceptButton = Me.AddButton
+        Me.AcceptButton = Me.AddItemButton
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.ExitButton
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LastNameLabel)
+        Me.Controls.Add(Me.FistNameLabel)
+        Me.Controls.Add(Me.LastNameTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
         Me.Controls.Add(Me.ExitButton)
-        Me.Controls.Add(Me.ClearButton)
-        Me.Controls.Add(Me.AddButton)
+        Me.Controls.Add(Me.ClearAllButton)
+        Me.Controls.Add(Me.AddItemButton)
         Me.Controls.Add(Me.DataListBox)
         Me.Name = "ListBoxForm"
         Me.Text = "List Box"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents DataListBox As ListBox
-    Friend WithEvents AddButton As Button
-    Friend WithEvents ClearButton As Button
+    Friend WithEvents AddItemButton As Button
+    Friend WithEvents ClearAllButton As Button
     Friend WithEvents ExitButton As Button
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNameTextBox As TextBox
+    Friend WithEvents FistNameLabel As Label
+    Friend WithEvents LastNameLabel As Label
 End Class
--- ListBoxExample/ListBoxForm.vb
+++ ListBoxExample/ListBoxForm.vb
@@ -1,3 +1,24 @@
 ﻿Public Class ListBoxForm
+    Private Sub ListBoxForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'DataListBox
+        FirstNameTextBox.Text = "Anakin"
+        LastNameTextBox.Text = "Skywalker"
+    End Sub
 
+    Private Sub AddItemButton_Click(sender As Object, e As EventArgs) Handles AddItemButton.Click
+        DataListBox.Items.Add(FirstNameTextBox.Text & " " & LastNameTextBox.Text)
+    End Sub
+
+    Private Sub ClearAllButton_Click(sender As Object, e As EventArgs) Handles ClearAllButton.Click
+        DataListBox.Items.Clear()
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub DataListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DataListBox.SelectedIndexChanged
+        Me.Text = DataListBox.SelectedIndex.ToString
+        'TO DO add selected to first name and last name text box
+    End Sub
 End Class
