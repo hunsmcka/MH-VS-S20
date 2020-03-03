ba0da7bddf33a9ce72f0e7871031321694865d51
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-03-03 08:56:45 -0700

    Created ArrayExample

---
+++ ArrayExample/ArrayExample.vb
@@ -0,0 +1,35 @@
+﻿Module ArrayExample
+
+    Sub Main()
+        Dim myArray(3) As Integer
+
+        myArray(1) = 5
+
+        myArray = {1, 2, 3, 4}
+
+        myArray(0) = 5
+
+        AnotherArrayExample()
+
+        Console.ReadLine()
+    End Sub
+
+    Sub AnotherArrayExample()
+        'no element definition
+        Dim infoArray() As String
+
+        infoArray = Split("Hello,Goodbye,Pizza,Hungry,Ice Cream", ",")
+
+        'For Each thingy In infoArray
+        '    Console.WriteLine(thingy)
+        '    infoArray.Contains(thingy)
+        'Next
+
+        For i = LBound(infoArray) To UBound(infoArray)
+            Console.WriteLine("At" & Str(i) & " Value is: " & infoArray(i))
+        Next
+
+        Console.ReadLine()
+
+    End Sub
+End Module
