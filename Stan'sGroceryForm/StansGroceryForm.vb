8f0afdec585c43659d47af34f28cefd88922e208
McKay Hunsaker <mckayhunsaker@gmail.com>
2020-04-25 18:34:27 -0600

    Created Stan's Grocery Form

---
+++ Stan'sGroceryForm/StansGroceryForm.vb
@@ -0,0 +1,18 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+'McKay Hunsaker
+'RCET 0265
+'Spring 2020
+'Stan's Grocery
+'https://github.com/hunsmcka/MH-VS-S20
+
+Public Class StansGroceryForm
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        If SearchTextBox.Text = "zzz" Then
+            Me.Close()
+        End If
+    End Sub
+End Class
