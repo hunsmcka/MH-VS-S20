3c879536b75b98198a8c808a53d3ef88707e6b4f
McKay Hunsaker <60007606+hunsmcka@users.noreply.github.com>
2020-04-27 15:38:02 -0600

    Add files via upload

---
+++ Stan'sGroceryForm/StansGroceryForm.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StansGroceryForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ItemComboBox = New System.Windows.Forms.ComboBox()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.ItemListBox = New System.Windows.Forms.ListBox()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.SearchLabel = New System.Windows.Forms.Label()
+        Me.ComboBoxLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ItemComboBox
+        '
+        Me.ItemComboBox.FormattingEnabled = True
+        Me.ItemComboBox.Location = New System.Drawing.Point(103, 87)
+        Me.ItemComboBox.Name = "ItemComboBox"
+        Me.ItemComboBox.Size = New System.Drawing.Size(257, 24)
+        Me.ItemComboBox.TabIndex = 1
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.Location = New System.Drawing.Point(103, 59)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(476, 22)
+        Me.SearchTextBox.TabIndex = 0
+        '
+        'ItemListBox
+        '
+        Me.ItemListBox.FormattingEnabled = True
+        Me.ItemListBox.ItemHeight = 16
+        Me.ItemListBox.Location = New System.Drawing.Point(366, 87)
+        Me.ItemListBox.Name = "ItemListBox"
+        Me.ItemListBox.Size = New System.Drawing.Size(294, 260)
+        Me.ItemListBox.TabIndex = 2
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(585, 59)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchButton.TabIndex = 3
+        Me.SearchButton.Text = "&Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'SearchLabel
+        '
+        Me.SearchLabel.AutoSize = True
+        Me.SearchLabel.Location = New System.Drawing.Point(6, 62)
+        Me.SearchLabel.Name = "SearchLabel"
+        Me.SearchLabel.Size = New System.Drawing.Size(91, 17)
+        Me.SearchLabel.TabIndex = 6
+        Me.SearchLabel.Text = "Look Up Item"
+        '
+        'ComboBoxLabel
+        '
+        Me.ComboBoxLabel.AutoSize = True
+        Me.ComboBoxLabel.Location = New System.Drawing.Point(20, 90)
+        Me.ComboBoxLabel.Name = "ComboBoxLabel"
+        Me.ComboBoxLabel.Size = New System.Drawing.Size(77, 17)
+        Me.ComboBoxLabel.TabIndex = 7
+        Me.ComboBoxLabel.Text = "Select Item"
+        '
+        'StansGroceryForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(701, 365)
+        Me.Controls.Add(Me.ComboBoxLabel)
+        Me.Controls.Add(Me.SearchLabel)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.ItemListBox)
+        Me.Controls.Add(Me.SearchTextBox)
+        Me.Controls.Add(Me.ItemComboBox)
+        Me.Name = "StansGroceryForm"
+        Me.Text = "Stan's Grocery"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ItemComboBox As ComboBox
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents ItemListBox As ListBox
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents SearchLabel As Label
+    Friend WithEvents ComboBoxLabel As Label
+End Class
---
+++ Stan'sGroceryForm/StansGroceryForm.vb
@@ -0,0 +1,101 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+'McKay Hunsaker
+'RCET 0265
+'Spring 2020
+'Stan's Grocery
+'https://github.com/hunsmcka/MH-VS-S20
+
+Public Class StansGroceryForm
+    Private itemsTextFile As String = "C:\Users\mckay\source\repos\MH-VS-S20\Stan'sGroceryForm\GroceryItems.txt"
+    Public itemInfo(2, 4) As String
+
+    Sub ReadFileToArray()
+        Dim numberAisle As Integer = 0
+
+        Try
+            FileOpen(1, itemsTextFile, OpenMode.Input)
+
+            Do While Not EOF(1)
+                Input(1, itemInfo(numberAisle, 0))
+                Input(1, itemInfo(numberAisle, 1))
+                Input(1, itemInfo(numberAisle, 2))
+                Input(1, itemInfo(numberAisle, 3))
+                Input(1, itemInfo(numberAisle, 4))
+                numberAisle += 1
+
+            Loop
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+
+        Finally
+            FileClose(1)
+
+        End Try
+
+        For i = 0 To 2
+            For j = 0 To 4
+                ItemComboBox.Items.Add(itemInfo(i, j))
+                ItemListBox.Items.Add(itemInfo(i, j))
+            Next
+        Next
+
+    End Sub
+
+    Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ReadFileToArray()
+    End Sub
+
+    Sub SearchItem()
+        Dim itemFound As Boolean
+        itemFound = False
+        Try
+
+            For i = 0 To 2
+                For j = 0 To 4
+                    If itemInfo(i, j) = SearchTextBox.Text Then
+                        MsgBox("You will find " & itemInfo(i, j) & " on asile " & (i + 1) & ", item number " & (j + 1))
+                        itemFound = True
+                    End If
+
+                Next
+
+            Next
+
+        Catch ex As Exception
+            MsgBox("Please enter a proper item.")
+
+        End Try
+
+        If itemFound = False Then
+            MsgBox("We're sorry this item was not found. Please select a different item.")
+        End If
+
+    End Sub
+
+    Private Sub SearchButton_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        If SearchTextBox.Text = "zzz" Then
+            Me.Close()
+
+        Else
+            SearchItem()
+
+        End If
+    End Sub
+
+    Private Sub ItemListBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ItemListBox.SelectedIndexChanged
+        SearchTextBox.Text = ItemListBox.SelectedItem.ToString
+
+    End Sub
+
+    Private Sub ItemComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ItemComboBox.SelectedIndexChanged
+        SearchTextBox.Text = ItemComboBox.SelectedItem.ToString
+
+    End Sub
+
+End Class
