14157361d76cc19ea0d77b7a44ca0ff2cd6be8c5
McKay Hunsaker <60007606+hunsmcka@users.noreply.github.com>
2020-05-04 10:46:15 -0600

    Finished the program

---
+++ Etch-o-Sketch/EtchoSketch.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EtchoSketchForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.DrawingPictureBox = New System.Windows.Forms.PictureBox()
+        Me.WaveformButton = New System.Windows.Forms.Button()
+        Me.EraseButton = New System.Windows.Forms.Button()
+        Me.ColorButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DrawingPictureBox
+        '
+        Me.DrawingPictureBox.BackColor = System.Drawing.SystemColors.HighlightText
+        Me.DrawingPictureBox.Location = New System.Drawing.Point(12, 12)
+        Me.DrawingPictureBox.Name = "DrawingPictureBox"
+        Me.DrawingPictureBox.Size = New System.Drawing.Size(809, 358)
+        Me.DrawingPictureBox.TabIndex = 0
+        Me.DrawingPictureBox.TabStop = False
+        '
+        'WaveformButton
+        '
+        Me.WaveformButton.Location = New System.Drawing.Point(451, 399)
+        Me.WaveformButton.Name = "WaveformButton"
+        Me.WaveformButton.Size = New System.Drawing.Size(119, 32)
+        Me.WaveformButton.TabIndex = 2
+        Me.WaveformButton.Text = "&Draw Waveform"
+        Me.WaveformButton.UseVisualStyleBackColor = True
+        '
+        'EraseButton
+        '
+        Me.EraseButton.Location = New System.Drawing.Point(80, 399)
+        Me.EraseButton.Name = "EraseButton"
+        Me.EraseButton.Size = New System.Drawing.Size(119, 32)
+        Me.EraseButton.TabIndex = 0
+        Me.EraseButton.Text = "&Erase"
+        Me.EraseButton.UseVisualStyleBackColor = True
+        '
+        'ColorButton
+        '
+        Me.ColorButton.Location = New System.Drawing.Point(262, 399)
+        Me.ColorButton.Name = "ColorButton"
+        Me.ColorButton.Size = New System.Drawing.Size(119, 32)
+        Me.ColorButton.TabIndex = 1
+        Me.ColorButton.Text = "&Change Color"
+        Me.ColorButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(634, 399)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(119, 32)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'EtchoSketchForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(833, 463)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.ColorButton)
+        Me.Controls.Add(Me.EraseButton)
+        Me.Controls.Add(Me.WaveformButton)
+        Me.Controls.Add(Me.DrawingPictureBox)
+        Me.Name = "EtchoSketchForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Etch-o-Sketch"
+        CType(Me.DrawingPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents DrawingPictureBox As PictureBox
+    Friend WithEvents WaveformButton As Button
+    Friend WithEvents EraseButton As Button
+    Friend WithEvents ColorButton As Button
+    Friend WithEvents ExitButton As Button
+End Class
---
+++ Etch-o-Sketch/EtchoSketch.vb
@@ -0,0 +1,84 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+'McKay Hunsaker
+'RCET 0265
+'Spring 2020
+'https://github.com//hunsmcka/MH-VS-S20
+Public Class EtchoSketchForm
+
+    Private Sub DrawingPictureBox_MouseMove(sender As Object, e As MouseEventArgs) Handles DrawingPictureBox.MouseMove
+
+        Dim graph As Graphics
+        Static oldX As Integer
+        Static oldY As Integer
+        Dim penColor As Color = ColorChange(False)
+        Dim myPen As New Pen(penColor)
+
+        graph = DrawingPictureBox.CreateGraphics
+
+        If e.Button.ToString = "Left" Then
+            'Draw line
+            graph.DrawLine(myPen, oldX, oldY, e.X, e.Y)
+
+        ElseIf e.Button.ToString = "Right" Then
+            'Clear graph
+            ClearGraphics()
+
+        ElseIf e.Button.ToString = "Middle" Then
+            'Open color dialog and change color to selected color
+            myPen.Color = ColorChange(True)
+
+        End If
+
+        oldX = e.X
+        oldY = e.Y
+
+        myPen.Dispose()
+        graph.Dispose()
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Closes the program
+        Me.Close()
+    End Sub
+
+    Private Sub ColorButton_Click(sender As Object, e As EventArgs) Handles ColorButton.Click
+        'Get access to color dialog
+        ColorChange(True)
+    End Sub
+
+    Function ColorChange(pickColor As Boolean) As Color
+        Static newColor As Color
+
+        If pickColor Then
+            'Open color dialog and change color to what is selected
+            ColorDialog1.ShowDialog()
+            newColor = ColorDialog1.Color
+
+        ElseIf newColor = Color.Empty Then
+            'When program starts set color to black
+            newColor = Color.Black
+
+        End If
+
+        Return newColor
+    End Function
+
+    Private Sub EraseButton_Click(sender As Object, e As EventArgs) Handles EraseButton.Click
+        'Clear graph
+        ClearGraphics()
+    End Sub
+
+    Sub ClearGraphics()
+        'Clear picture box
+        DrawingPictureBox.Refresh()
+    End Sub
+
+    Private Sub WaveformButton_Click(sender As Object, e As EventArgs) Handles WaveformButton.Click
+        MsgBox("Unsupported Feature")
+    End Sub
+End Class
